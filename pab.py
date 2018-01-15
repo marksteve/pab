a3eecc80c87f600782dabb4960b848fc400af2f1
Mark Steve Samson <hello@marksteve.com>
2018-01-15 15:02:48 +0800

    Stuff

--- pab.py
+++ pab.py
@@ -8,7 +8,7 @@ from dotenv import find_dotenv, load_dotenv
 
 TEXT_TEMPLATE = '''
 Slot open for *{}* on *{}*!
-[Book now!](https://www.passport.gov.ph/appointment/individual/schedule)
+[Schedule an appointment](https://www.passport.gov.ph/appointment/individual/schedule)
 '''.strip()
 
 
